bd438b0ecf4fc01ca2d1c76a6d0b42b7edf61ca3
thamerh <hamdithamer99@gmail.com>
2022-08-31 12:58:55 +0100

    feat: add route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Singup";
+import { Fragment } from "react";
+import { BrowserRouter,Route,Redirect} from "react-router-dom";
+import './App.css'
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const user = localStorage.getItem("token");
+	return (
+    
+    <Fragment>
+      <BrowserRouter>
+      <Route  path="/signup" ><Signup/></Route>
+		 	<Route path="/login" ><Login/></Route>
+       {user && <Route path="/" ><Home/></Route>} 
+			{! user && <Route  exact path="/" > <Redirect  to="/login" /></Route>}
+      </BrowserRouter>
+    </Fragment>
+
+	);
 }
 
 export default App;
