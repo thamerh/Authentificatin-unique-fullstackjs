c3af3e3ebc85fdfed086d9e72600b89b70e4f255
thamerh <hamdithamer99@gmail.com>
2022-09-02 14:12:15 +0100

    fix: fixed routing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 import express from "express";
-import {Login,Register} from "../controllers/User.js"
-const router = express.Router();
+import { Register, Login, Logout } from "../controllers/Users.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
-router.post('/Login', Login);
-router.post('/Register', Register);
+const router = express.Router();
+router.post('/users', Register);
+router.post('/login', Login);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+
 export default router;
