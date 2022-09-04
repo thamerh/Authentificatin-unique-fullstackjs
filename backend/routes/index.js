13568db6d09b7b479852956fb8dc8b1a8287d99d
thamerh <hamdithamer99@gmail.com>
2022-09-04 12:23:40 +0100

    feat: integrate UploadFile controllers features in server routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,16 +3,25 @@ import { Register, Login, Logout } from "../controllers/Users.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import { getStudent,getStudentById,updateStudent,deleteStudent,addStudent } from "../controllers/CRUD.js";
 import {verifyToken} from "../middleware/verifyToken.js";
-
+import {addFile,getFiles,getOneFile,updateFile,upload,deleteFile,DownolodsFile} from "../controllers/UploadFile.js"
 const router = express.Router();
 router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+//Application one : CRUD
 router.post('/students', addStudent)
 router.get('/students' , verifyToken,getStudent );
 router.get('/students/:id', verifyToken,getStudentById);
 router.patch('/students/:id', updateStudent);
 router.delete('/students/:id',deleteStudent);
+//Application tow : CRUD file
+router.get('/DownolodsFile/:file', DownolodsFile);
+router.post('/addFile',upload,addFile);
+router.get('/files' , verifyToken, getFiles );
+router.get('/files/:id', verifyToken,getOneFile);
+router.put('/files/:id',upload, updateFile);
+router.delete('/files/:id', deleteFile);
+
 
 export default router;
