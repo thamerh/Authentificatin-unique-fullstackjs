eccf3520d539363c2877340ae37f366ebb36dfcc
thamerh <hamdithamer99@gmail.com>
2022-09-04 12:09:01 +0100

    feat: import bootstrap in index.js

--- frontend/uplod-display-delete-download-file/src/index.js
+++ frontend/uplod-display-delete-download-file/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "bulma/css/bulma.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
