cded125eeeeb15562a397e3b91454580d3d458a7
thamerh <hamdithamer99@gmail.com>
2022-09-04 12:13:09 +0100

    feat: integrate AddFile,EditFile and FileDetail components in App.js router

--- frontend/uplod-display-delete-download-file/src/App.js
+++ frontend/uplod-display-delete-download-file/src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import AddFile from "./components/AddFile";
+import EditFile from "./components/EditFile";
+import { FileDetail } from "./components/FileDetail";
+import ShowFile from "./components/ShowFile";
 
 function App() {
   return (
@@ -16,6 +20,9 @@ function App() {
         <Route exact path="/">
           <Dashboard/>
         </Route>
+        <Route path="/AddFile">< AddFile /></Route>
+        <Route path="/Detail/edit/:id"> <EditFile/></Route>
+        <Route path="/Detail/:id"><FileDetail/> </Route>
       </Switch>
     </BrowserRouter>
   );
